edd555a5871defe64ebde2a31b178b8f0cec4455
SolomkaIevgenii <solomka1989@gmail.com>
2018-07-12 18:15:24 +0300

    Little big harder than "Hello World"

--- app/src/main/java/com/example/john/parking2c/MainActivity.java
+++ app/src/main/java/com/example/john/parking2c/MainActivity.java
@@ -2,31 +2,24 @@ package com.example.john.parking2c;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.net.URL;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
-    private void checkPayment(){
-        final EditText carNumber = findViewById(R.id.CarNumber);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Номер авто: "+carNumber.getText());
-        builder.setMessage("Парковка не оплачена");
-        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+    String result;
+    EditText carNumber;
 
-            }
-        });
-        builder.setCancelable(false);
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +29,44 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonSend).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPayment();
+
+                AsyncHttpClient client = new AsyncHttpClient();
+
+                carNumber = findViewById(R.id.CarNumber);
+                final String url = "http://parking.2click.money/ExtApi/carState?id="+carNumber.getText()+"&accToken=fdf909e4j3f03jikdsjfpsdg9sdfd0ifjsdik";
+
+                client.get(url, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        assert result != null;
+                        result = new String(responseBody);
+                        checkPayment();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        assert result != null;
+                        result = "Ошибка на сервере";
+
+                    }
+                });
+
+
             }
         });
+    }
+    private void checkPayment(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Номер авто: "+carNumber.getText());
+        builder.setMessage(result);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setCancelable(false);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
